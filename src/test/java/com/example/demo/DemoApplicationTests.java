dd861859c942c2ee704ca25b95331f1b9dd74d22
hector <hencinas23@gmail.com>
2026-01-20 11:25:46 +0100

    .

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.controller.UsuarioController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UsuarioController usuarioController;
+
+    @Test
+    void contextLoads() {
+        // Verifica que el controlador se haya cargado correctamente en el contexto
+        assertThat(usuarioController).isNotNull();
+    }
+
 
 }
